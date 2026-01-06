bd7e94de84be401b75db58148056cc1a5da23919
Android-Piepaint <2163971449v@gmail.com>
2026-01-06 16:02:10 +0800

    Update: Added ROC calender.

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,3 +1,12 @@
 export function formatDateToYYYYMMDD(date: Date): string {
 	return date.toISOString().substring(0, 10);
 }
+export function formatDateMinguo(dateInput: string | Date): string {
+  const date = new Date(dateInput)
+
+  const year = date.getFullYear() - 1911
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return `民國 ${year} 年 ${month} 月 ${day} 日`
+}
